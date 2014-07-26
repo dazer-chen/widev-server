af304accb86e7050bd197960a6091faf1ef78582
Gaetan SENN <ridertahiti@hotmail.com>
2014-07-26 18:09:19 +0200

    Updated user information

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,7 +5,13 @@ import reactivemongo.bson.{Macros, BSONObjectID}
 /**
  * Created by trupin on 7/26/14.
  */
-case class User(_id: BSONObjectID = BSONObjectID.generate, name: String, hashedPassword: String)
+case class User(_id: BSONObjectID = BSONObjectID.generate,
+                email: String,
+                first_name: String,
+                last_name: String,
+                name: String,
+                password: String,
+                github: String)
 
 object User {
   implicit val handler = Macros.handler[User]
