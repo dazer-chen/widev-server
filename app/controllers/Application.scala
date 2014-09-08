e0e71849926c026817a172b3be6e3b116eea583e
trupin <theophane.rupin@gmail.com>
2014-09-08 20:05:16 +0200

    add s3 test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import fly.play.aws.auth.AwsCredentials
+import fly.play.s3.{BucketFile, S3}
 import jp.t2v.lab.play2.auth.AuthElement
 import models._
 import play.api.libs.json.Json
@@ -21,6 +23,18 @@ object Application extends Controller with AuthElement with AuthConfigImpl {
 
   def AuthenticateTest = StackAction(AuthorityKey -> Visitor) { response =>
     Ok("ok")
+  }
+
+  def getFileTest = Action.async {
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    val bucket = S3("widev-fs")(AwsCredentials.apply("AKIAJJFHVLQ6BJL4K3CA", "i/VNaA7GLzPcl48G0Vlrnq3YvIBbjtalzeSAyXAG"))
+
+    bucket.get("id.jpg").map {
+      case BucketFile(name, contentType, content, acl, headers) =>
+        Ok(content)
+    }
+
   }
 
 }
