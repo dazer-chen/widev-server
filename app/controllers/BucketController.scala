5a7cce5d09c6a81766457b786153dec3f171a8f8
trupin <theophane.rupin@gmail.com>
2014-09-13 10:34:05 +0200

    added bucket

--- app/controllers/BucketController.scala
+++ app/controllers/BucketController.scala
@@ -1,15 +1,21 @@
 package controllers
 
+import fly.play.aws.PlayConfiguration
+import fly.play.aws.auth.AwsCredentials
+import fly.play.s3.{BucketFile, S3}
 import jp.t2v.lab.play2.auth.AuthElement
 import lib.mongo.DuplicateModel
-import models.{User, Standard, Bucket, Buckets}
+import models.{Bucket, Buckets, Standard}
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.{JsError, JsPath, Reads, Json}
-import play.api.mvc.{Action, BodyParsers, Controller}
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+import play.api.libs.json._
+import play.api.mvc.{BodyParsers, Controller}
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import reactivemongo.bson.BSONObjectID
-import play.api.libs.functional.syntax._
 
 import scala.concurrent.Future
 
@@ -20,11 +26,13 @@ import scala.concurrent.Future
 class BucketController(buckets: Buckets) extends Controller with AuthElement {
   self: AuthConfigImpl =>
 
+  val s3Bucket = S3(PlayConfiguration("s3.bucket"))(AwsCredentials.fromConfiguration)
+
   def getBucket(id: String) = AsyncStack(AuthorityKey -> Standard) {
     request =>
       buckets.find(BSONObjectID(id)).map {
         case Some(bucket) => Ok(Json.toJson(bucket))
-        case None => NotFound(s"Couldn't find workspace for id: $id")
+        case None => NotFound(s"Couldn't find bucket for id: $id")
       }
   }
 
@@ -41,11 +49,12 @@ class BucketController(buckets: Buckets) extends Controller with AuthElement {
 
       bucket.fold(
         errors => {
-          Future(BadRequest(Json.obj("status" -> "KO", "message" -> JsError.toFlatJson(errors))))
+          Future(BadRequest(JsError.toFlatJson(errors)))
         },
         bucket => {
           buckets.create(Bucket(bucket.name, BSONObjectID(bucket.owner))).map {
-            bucket => Ok(Json.toJson(bucket))
+            bucket =>
+              Ok(Json.toJson(bucket))
           } recover {
             case err: DuplicateModel =>
               NotAcceptable(s"User already exists.")
@@ -53,6 +62,54 @@ class BucketController(buckets: Buckets) extends Controller with AuthElement {
         }
       )
     }
+
+  def syncBucket(id: String) = AsyncStack(BodyParsers.parse.json, AuthorityKey -> Standard) {
+    request =>
+      case class SyncBucketItem(path: String, md5: String, lastUpdate: DateTime)
+
+      implicit val syncBucketItemsReads: Reads[SyncBucketItem] = (
+        (JsPath \ "path").read[String] and
+          (JsPath \ "md5").read[String] and
+          (JsPath \ "lastUpdate").read[DateTime]
+        )(SyncBucketItem.apply _)
+
+      request.body.validate[Seq[SyncBucketItem]].fold(
+        errors => {
+          Future.successful(BadRequest(JsError.toFlatJson(errors)))
+        },
+        bucketItems => {
+
+          buckets.find(BSONObjectID(id)).flatMap {
+            case Some(bucket) =>
+              Future.sequence(bucketItems.map {
+                item =>
+                  s3Bucket.get(bucket.name + "/" + item.path).map {
+                    case BucketFile(name, contentType, content, acl, headers) =>
+                      val dtf = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'")
+                      DateTime.parse(headers.get("Last-Modified"), dtf)
+//                      println(headers)
+                      name
+                  }
+              }).map {
+                list => Ok(Json.toJson(list))
+              }
+            case None => Future(NotFound(s"Couldn't find bucket for id: $id"))
+          }
+        }
+      )
+
+//      buckets.find(BSONObjectID(id)).flatMap {
+//        case Some(bucket) =>
+//          s3Bucket.list(s"${bucket.name}/").map {
+//            it =>
+//              Ok(Json.toJson(it.map {
+//                item => item.name
+//              }))
+//          }
+//        case None => Future(NotFound(s"Couldn't find bucket for id: $id"))
+//      }
+  }
+
 }
 
 object BucketController extends BucketController(Buckets(ReactiveMongoPlugin.db)) with AuthConfigImpl
