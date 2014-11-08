78bbffdb1dac0eff816f29bc7faaf9afacfbc4be
trupin <theophane.rupin@gmail.com>
2014-11-08 20:48:24 +0100

    fixed compilation error

--- app/managers/PluginManager.scala
+++ app/managers/PluginManager.scala
@@ -4,21 +4,27 @@ import models.Plugins
 import play.api.libs.ws.WS
 import play.api.mvc.Request
 import play.modules.reactivemongo.ReactiveMongoPlugin
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.concurrent.Future
 
 /**
  * Created by trupin on 11/8/14.
  */
 class PluginManager(plugins: Plugins) {
 
-  def broadcastToAll(request: Request) = plugins.list.map(_.foreach {
-    plugin =>
-      WS.url(plugin.endPoint + request.path)
-        .withHeaders(request.headers.toSimpleMap.toSeq:_*)
-        .withRequestTimeout(10000)
-        .withQueryString(request.queryString.toSeq.map { case (k, v) => (k, v.mkString(",")) }:_*)
-        .execute(request.method)
-  })
-
+  def broadcastToAll[T](request: Request[T]) = plugins.list.flatMap {
+    plugins =>
+      Future.sequence(plugins.map {
+        plugin =>
+          WS.url(plugin.endPoint + request.path)
+            .withHeaders(request.headers.toSimpleMap.toSeq:_*)
+            .withRequestTimeout(10000)
+            .withQueryString(request.queryString.toSeq.map { case (k, v) => (k, v.mkString(",")) }:_*)
+            .execute(request.method)
+      }).mapTo[Unit]
+  }
 }
 
 object PluginManager extends PluginManager(new Plugins(ReactiveMongoPlugin.db))
